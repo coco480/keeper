0e7aac2cd11ba60f09393eab3ac21aef33a3ee92
coco <coco480@gmail.com>
2022-02-15 06:05:24 -0500

    Keeper Part1 comptete

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const year = new Date().getFullYear();
+
+const Footer = () => {
+    return <footer><p>Copyright &copy; {year}</p></footer>;
+};
+
+export default Footer;
